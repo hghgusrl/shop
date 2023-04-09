0f9ab80ebb9a3b77c634c281e07cf13290327890
hhj <hghgusrl@naver.com>
2023-04-09 14:25:11 +0900

    thymeleaf for문

---
+++ src/main/java/com/shop/dto/ItemDto.java
@@ -0,0 +1,29 @@
+package com.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class ItemDto {
+
+    private Long id;
+
+    private String itemNm;
+
+    private Integer price;
+
+    private String itemDetail;
+
+    private String sellStatCd;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+}
